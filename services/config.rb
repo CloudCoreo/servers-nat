7ff07e116779887bf0a3cfe244686e0306d6ae4c
Kim Viacheslav <kimslava93@gmail.com>
2016-12-17 10:55:56 +0600

    region is set to be the same as in plan was set. AMI version fetched from servers-base

--- services/config.rb
+++ services/config.rb
@@ -1,35 +1,34 @@
-
 coreo_aws_ec2_securityGroups "${NAT_SG_NAME}${SUFFIX}" do
   action :sustain
   description "NAT security group"
   vpc "${VPC_NAME}${SUFFIX}"
-  allows [ 
-          { 
-            :direction => :ingress,
-            :protocol => :tcp,
-            :ports => ${NAT_INGRESS_PORTS},
-            :cidrs => ${NAT_INGRESS_CIDRS}
-          },
-          { 
-            :direction => :egress,
-            :protocol => :tcp,
-            :ports => ${NAT_EGRESS_PORTS},
-            :cidrs => ${NAT_EGRESS_CIDRS}
-          },
-          { 
-            :direction => :ingress,
-            :protocol => :udp,
-            :ports => [123],
-            :cidrs => ["0.0.0.0/0"],
-          },
-          { 
-            :direction => :egress,
-            :protocol => :udp,
-            :ports => [123],
-            :cidrs => ["10.0.0.0/8"],
-          }
-    ]
-    region "${REGION}"
+  allows [
+             {
+                 :direction => :ingress,
+                 :protocol => :tcp,
+                 :ports => $ {NAT_INGRESS_PORTS},
+      :cidrs => $ {NAT_INGRESS_CIDRS}
+  },
+      {
+          :direction => :egress,
+          :protocol => :tcp,
+          :ports => $ {NAT_EGRESS_PORTS},
+      :cidrs => $ {NAT_EGRESS_CIDRS}
+  },
+      {
+          :direction => :ingress,
+          :protocol => :udp,
+          :ports => [123],
+          :cidrs => ["0.0.0.0/0"],
+      },
+      {
+          :direction => :egress,
+          :protocol => :udp,
+          :ports => [123],
+          :cidrs => ["10.0.0.0/8"],
+      }
+  ]
+  region "${PLAN::region}"
 end
 
 coreo_aws_iam_policy "${NAT_NAME}" do
@@ -70,7 +69,7 @@ coreo_aws_iam_policy "${NAT_NAME}" do
     }
   ]
 }
-EOH
+  EOH
 end
 
 coreo_aws_iam_instance_profile "${NAT_NAME}" do
@@ -80,20 +79,20 @@ end
 
 coreo_aws_ec2_instance "${NAT_NAME}${SUFFIX}" do
   action :define
-  image_id "${NAT_AMI}"
+  image_id "${AWS_LINUX_AMI}"
   size "${NAT_SIZE}"
   security_groups ["${NAT_SG_NAME}${SUFFIX}"]
   associate_public_ip true
   role "${NAT_NAME}"
   ssh_key "${NAT_KEY}"
-  region "${REGION}"
+  region "${PLAN::region}"
 end
 
 coreo_aws_ec2_autoscaling "${NAT_NAME}${SUFFIX}" do
-  action :sustain 
+  action :sustain
   minimum ${NAT_GROUP_SIZE_MIN}
   maximum ${NAT_GROUP_SIZE_MAX}
   server_definition "${NAT_NAME}${SUFFIX}"
   subnet "${PUBLIC_SUBNET_NAME}${SUFFIX}"
-  region "${REGION}"
+  region "${PLAN::region}"
 end
